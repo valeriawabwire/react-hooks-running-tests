0c985ea7c4145727d5275a77810d05df91452c73
valeria wabwire <larriebliss@gmail.com>
2024-04-28 12:18:20 +0300

    react hooks running tests

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,11 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+       please pass this test 
     </div>
   );
 }
+//screen.debug();
+//expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 
 export default Article;
